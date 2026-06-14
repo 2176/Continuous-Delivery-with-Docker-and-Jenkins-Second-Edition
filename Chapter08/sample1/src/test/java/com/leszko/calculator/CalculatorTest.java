113f729296b89ab1f191c9bd997659ea49dc2ed0
Moses <Moses_Basaza@student.uml.edu>
2026-06-13 22:08:28 -0400

    Fix Checkstyle in calculator test

--- Chapter08/sample1/src/test/java/com/leszko/calculator/CalculatorTest.java
+++ Chapter08/sample1/src/test/java/com/leszko/calculator/CalculatorTest.java
@@ -1,10 +1,11 @@
 package com.leszko.calculator;
 
+import org.junit.Test;
+import org.springframework.test.util.ReflectionTestUtils;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-import org.junit.Test;
-import org.springframework.test.util.ReflectionTestUtils;
 /**
  * Tests for calculator behavior.
  */
