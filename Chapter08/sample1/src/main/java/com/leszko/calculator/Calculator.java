242edcb5d290d7df14e8366a37a88c280e0e5d53
Moses <Moses_Basaza@student.uml.edu>
2026-06-20 13:03:45 -0400

    Add Javadoc for calculator class

--- Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
+++ Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
@@ -2,7 +2,9 @@ package com.leszko.calculator;
 
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
-
+/**
+ * Provides calculator operations.
+ */
 @Service
 public class Calculator {
         final static int umlNUMBER1 = 3;
